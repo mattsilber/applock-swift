745e797aafde6012b79e0bfe3a853fcaf4f48bf9
Matt Silber <cruceo@guardanis.com>
2017-11-09 18:38:04 -0500

    Initial Commit

---
+++ SwiftyAppLock/SwiftyAppLock.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyAppLock.h
+//  SwiftyAppLock
+//
+//  Created by Matthew Silber on 11/9/17.
+//  Copyright © 2017 Guardanis LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyAppLock.
+FOUNDATION_EXPORT double SwiftyAppLockVersionNumber;
+
+//! Project version string for SwiftyAppLock.
+FOUNDATION_EXPORT const unsigned char SwiftyAppLockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyAppLock/PublicHeader.h>
+
+
